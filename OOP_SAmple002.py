83a2ce72bac60336a180cf5b6eeebc2c82f3e5d1
Ajay S <ajaysimionin@gmail.com>
2023-06-05 17:04:45 +0530

    removed s2

--- OOP_SAmple002.py
+++
@@ -1,9 +0,0 @@
-class Odd_Even:
-    def check(self,num):
-        if num%2==0:
-            print("It is a Even Number")
-        else:
-            print("It is a Odd Number")
-n=Odd_Even()
-x=int(input("Enter a number : "))
-n.check(x)
